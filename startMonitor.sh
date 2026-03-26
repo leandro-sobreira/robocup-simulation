093a0b6e2713e4ce4f7410ff83a4f2e99f6997d7
Leandro Sobreira <lperes16623@gmail.com>
2026-03-26 22:51:40 +0000

    AgoraFoi

--- startMonitor.sh
+++ startMonitor.sh
@@ -1,2 +1,6 @@
+INSTALL_DIR="/workspaces/$(basename $(pwd))/installs"
+export PATH="$INSTALL_DIR/bin:$PATH"
+export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH"
+
 echo "Iniciando RCSSMonitor..."
-/workspaces/robocup-simulation/installs/bin/rcssmonitor
+rcssmonitor &
--- startServer.sh
+++ startServer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+INSTALL_DIR="/workspaces/$(basename $(pwd))/installs"
+export PATH="$INSTALL_DIR/bin:$PATH"
+export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH"
 
 # Inicializa a interface gráfica (VNC)
 echo "Iniciando a interface gráfica..."
@@ -7,9 +10,12 @@ echo "Iniciando a interface gráfica..."
 # Espera 2 segundos para o sistema gráfico subir
 sleep 2
 
+pkill -9 rcssserver || true
+pkill -9 rcssmonitor || true
+
 # Inicia o Servidor em segundo plano
 echo "Iniciando RCSSServer..."
-/workspaces/robocup-simulation/installs/bin/rcssserver
+rcssserver &
 
 
 echo "Tudo pronto! Verifique a aba do navegador na porta 6080, e inicie "startMonitor.sh" em outro terminal."
