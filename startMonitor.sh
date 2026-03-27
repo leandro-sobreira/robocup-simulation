16ae362eff52aa5507f3c712a5c982dc600fa59a
Leandro Sobreira <lperes16623@gmail.com>
2026-03-27 18:13:46 +0000

    Helios-Base

--- startMonitor.sh
+++
@@ -1,6 +0,0 @@
-INSTALL_DIR="/workspaces/$(basename $(pwd))/installs"
-export PATH="$INSTALL_DIR/bin:$PATH"
-export LD_LIBRARY_PATH="$INSTALL_DIR/lib:$LD_LIBRARY_PATH"
-
-echo "Iniciando RCSSMonitor..."
-rcssmonitor &
